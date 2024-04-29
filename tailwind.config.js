f2359d764d5bbee5519d5c99ab0b9c78715cb860
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-04-29 12:36:10 +0700

    feat: :tada: Project Initialization
    Start kanban project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sf-400": ["SF-400"],
+        "sf-500": ["SF-500"],
+        "sf-600": ["SF-600"],
+        "sf-700": ["SF-700"],
+      },
+      colors: {
+        "dark-grey": "#4D4D4D",
+        "light-grey": "#E1E4E8",
+        rose: "#FFDCE0",
+        green: "#CBDFD8",
+        lavender: "#F0E7F6",
+      },
+    },
+  },
+  plugins: [],
+};
