66c6dfd32d5eea4533062b5142e92847c5b2ced5
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-04-29 22:09:26 +0700

    move button

--- src/containers/Kanban/index.tsx
+++ src/containers/Kanban/index.tsx
@@ -3,16 +3,16 @@ import { Board, Text } from "components";
 function Kanban({ title, desc, listData, setData, data }: KanbanProps) {
   return (
     <main className="max-w-screen h-screen">
-      <Text type="title" text={title} />
-      <Text type="normal" text={desc} />
-      <br />
-      <Board listData={listData} />
       <button
-        className="mt-6 text-base bg-dark-grey text-white py-1 px-3 rounded-lg absolute bottom-4 left-4"
+        className="mt-6 text-base bg-dark-grey text-white py-1 px-3 rounded-lg absolute top-5 right-8"
         onClick={() => setData(data ? false : true)}
       >
         Change Data
       </button>
+      <Text type="title" text={title} />
+      <Text type="normal" text={desc} />
+      <br />
+      <Board listData={listData} />
     </main>
   );
 }
